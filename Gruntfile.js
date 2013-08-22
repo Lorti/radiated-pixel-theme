a8e9254a7a32a2fe402c9fd6174b68365bdf2c83
Manuel Wieser <office@manuelwieser.com>
2013-08-22 16:50:21 +0200

    live reload port set back to default

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,7 +15,7 @@ module.exports = function(grunt) {
         files: 'scss/**',
         tasks: ['compass'],
         options: {
-          livereload: 1337
+          livereload: true
         }
       }
     }
