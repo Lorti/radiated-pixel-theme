5c4b86aaa53ec44f1a30150abfc56525d176c34f
Manuel Wieser <office@manuelwieser.com>
2013-10-01 16:16:46 +0200

    placeholder of the search form changed to Search..., deleted an inline style in the header

--- searchform.php
+++ searchform.php
@@ -7,6 +7,6 @@
 ?>
 	<form method="get" id="searchform" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 		<label for="s" class="search-form__screen-reader-text"><?php _ex( 'Search', 'assistive text', 'rp' ); ?></label>
-		<input type="search" class="search-form__field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Looking for something?', 'placeholder', 'rp' ); ?>" />
+		<input type="search" class="search-form__field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="<?php echo esc_attr_x( 'Search&hellip;', 'placeholder', 'rp' ); ?>" />
 		<input type="submit" class="search_form__submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'rp' ); ?>" />
 	</form>
