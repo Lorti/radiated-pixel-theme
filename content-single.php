b772482387e5f02075f954425c0c2fc6ec0c4ba7
Manuel Wieser <office@manuelwieser.com>
2013-10-21 19:49:30 +0200

    disqus comments enabled

--- content-single.php
+++ content-single.php
@@ -49,6 +49,11 @@
 					);
 				?>
 			</small>
+	    <?php
+	      // If comments are open or we have at least one comment, load up the comment template
+	      if ( comments_open() || '0' != get_comments_number() )
+	        comments_template();
+	    ?>
 		</footer><!-- .entry-meta -->
 
 	</div>
